c68bad54b3cb0f53d1409ccf8c96f583a196c1b4
mike witter <thomas.marler@hotmail.com>
2017-08-17 15:16:50 -0500

    Created ProPublica class and created methods

--- Controller/ProPublica_Controller.py
+++ Controller/ProPublica_Controller.py
@@ -3,24 +3,63 @@ import requests
 # https://api.propublica.org/congress/v1/members/senate/RI/api-key=FuMVFXTU3fJHtKTHC9v480ZXmampcvt6J0vzYBji/current.json
 # https://api.propublica.org/congress/v1/members/senate/MO/api-key=FuMVFXTU3fJHtKTHC9v480ZXmampcvt6J0vzYBji/current.json
 
-def getSentatorByState(state):
-    return 'https://api.propublica.org/congress/v1/members/senate/' + state +'/api-key=FuMVFXTU3fJHtKTHC9v480ZXmampcvt6J0vzYBji/current.json'
+class ProPublica(object):
+    
+    def __init__(self):
+        self._key = "FuMVFXTU3fJHtKTHC9v480ZXmampcvt6J0vzYBji"
+        self._senatorBaseURL = "https://api.propublica.org/congress/v1/members/senate"
+        self._houseBaseURL = "https://api.propublica.org/congress/v1/members/house"
+        self._specificMemberURL = "https://api.propublica.org/congress/v1/members"
+        self._specificBillURL = "https://api.propublica.org/congress/v1"
 
-def listSentatorByState(state):
+    def listSentatorByState(self, state):
 
-    url = "https://api.propublica.org/congress/v1/members/senate/" +state+"/api-key=FuMVFXTU3fJHtKTHC9v480ZXmampcvt6J0vzYBji/current.json"
+        url = self._senatorBaseURL + "/" +state+ "/current.json"
+        headers = {
+            'x-api-key': self._key
+        }
+        response = requests.request("GET", url, headers=headers)
 
-    headers = {
-        'x-api-key': "FuMVFXTU3fJHtKTHC9v480ZXmampcvt6J0vzYBji",
-        'cache-control': "no-cache",
-        'postman-token': "84558452-50c3-23a9-85f7-e1db4c66735f"
-    }
+        if response.status_code != 200:
+            print('Something went wrong'.format(response.status_code))
+        else:
+            return response.json()
 
-    response = requests.request("GET", url, headers=headers)
-    return response.json()
-    # if resp.status_code != 200:
-    #     print('Something went wrong'.format(resp.status_code))
-    # else:
-    #     # print("This is the json")
-    #     return resp.json()
+    def listHouseOfRepsByState(self, state):
 
+        url = self._houseBaseURL + "/" +state+ "/current.json"
+        headers = {
+            'x-api-key': self._key
+        }
+        response = requests.request("GET", url, headers=headers)
+
+        if response.status_code != 200:
+            print('Something went wrong'.format(response.status_code))
+        else:
+            return response.json()
+
+    def repDetails(self, member_id):
+
+        url = self._specificMemberURL + "/" +member_id+ "/votes.json"
+        headers = {
+            'x-api-key': self._key
+        }
+        response = requests.request("GET", url, headers=headers)
+        if response.status_code != 200:
+            print('Something went wrong'.format(response.status_code))
+        else:
+            return response.json()
+
+    def billDetails(self, congress, bill_id):
+        """ Congress 105-115 """
+
+        url = self._specificBillURL + "/" + congress + "/bills/" + bill_id + ".json"
+        headers = {
+            'x-api-key': self._key
+        }
+
+        response = requests.request("GET", url, headers=headers)
+        if response.status_code != 200:
+            print('Something went wrong'.format(response.status_code))
+        else:
+            return response.json()
--- Manager/ProPublica_Manager.py
+++ Manager/ProPublica_Manager.py
@@ -1,10 +1,31 @@
-from Controller.ProPublica_Controller import listSentatorByState
+from Controller.ProPublica_Controller import ProPublica
+import pprint
 
-def getSources():
-    s = listSentatorByState('MO')
-    print("This is from the manager: ")
-    print(s)
+class ProPublicaManager(object):
+    def __init__(self):
+        self._api = ProPublica()
+
+    def getListOfStateSenator(self):
+        jsonOfStateSentator = self._api.listSentatorByState('MO')
+        return jsonOfStateSentator
+
+    def getListOfStateHouseReps(self):
+        jsonOfHouseReps = self._api.listHouseOfRepsByState('MO')
+        return jsonOfHouseReps
+
+    def getRepDetails(self):
+        jsonOfRepDetails = self._api.repDetails('C001049')
+        return jsonOfRepDetails
+
+    def getBillDetails(self):
+        jsonOfBillDetails = self._api.billDetails('115', 'hr2810')
+        return jsonOfBillDetails
 
 
 if __name__ == "__main__":
-    getSources()
+    api = ProPublicaManager()
+    senator = api.getListOfStateSenator()
+    house = api.getListOfStateHouseReps()
+    details = api.getRepDetails()
+    billDetails = api.getBillDetails()
+    pprint.pprint(billDetails)
