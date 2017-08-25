828f7f99496a33642b684780855735b96030af77
tommarler <thomas.marler@hotmail.com>
2017-08-25 15:23:22 -0500

    Created ProPublicaManager to process the json data from the Propublica members class, will need to run NTLK on the text and also scrub the data

--- Manager/ProPublica_Manager.py
+++ Manager/ProPublica_Manager.py
@@ -1,9 +1,20 @@
 from Controller.ProPublica.Members import Members
+from Controller.Twitter.TwitterController import Twitter
 
 class ProPublicaManager(object):
 
     def __init__(self):
         self.member = Members()
+        self.twitter = Twitter()
 
-    def g(self):
-        a = self.member.input.ge
+    def stateHouseRepsData(self):
+        member = Members()
+        house = member.getListOfStateHouseReps()
+        parsedHouseJson = member.parseHouse(house)
+        member.printHouse(parsedHouseJson)
+        return parsedHouseJson
+
+
+if __name__ == "__main__":
+    i = ProPublicaManager()
+    i.stateHouseRepsData()
